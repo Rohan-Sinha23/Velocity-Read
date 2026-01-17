6b69bade1ed86654b6f1df347490f11c0243cbf9
Rohan-Sinha23 <rohansinha.210523@gmail.com>
2026-01-18 03:33:00 +0530

    Add files via upload

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Velocity-Read/sw.js', { scope: '/Velocity-Read/' })})}
